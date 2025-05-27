08f013e83c27f53ba254efb574ccfe3159a466b2
LAMIYA ALIZADA <lamiyaea@code.edu.az>
2025-05-27 19:28:31 +0400

    coomit

--- src/sections/Home/PopularSection/index.jsx
+++ src/sections/Home/PopularSection/index.jsx
@@ -60,7 +60,7 @@ const PopularSection = () => {
                 <ArrowRight className='text-[#e52e06] w-[16px] h-[16px]' />
             </ul>
         </div>
-        <div className='my-10 grid grid-cols-1 md:grid-cols-5 sm:grid-cols-2 lg:grid-cols-3'>
+        <div className='my-10 grid grid-cols-5'>
 
         {
                    productSection&&productSection.map((item,idx)=>(
